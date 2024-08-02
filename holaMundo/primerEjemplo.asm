790ae97112f33aa87727db9c1f8e78da321ed6db
Daniel <159450603+DarchoG@users.noreply.github.com>
2024-08-02 04:30:22 -0600

    Create primerEjemplo.asm

---
+++ holaMundo/primerEjemplo.asm
@@ -0,0 +1,13 @@
+org 100h ; Empezar en la direccion 100h.
+    
+   jmp start ; Salto incondiconal, por lo tanto siempre ocurre, para evitar ejecutar. 
+
+   string db "Hello World", "$" ; Las variables necesitan disponer de un "$" al final para ser mostradas en pantalla con la interrupcion.
+   
+   start: ; Etiqueta definida para realizar saltos.
+   
+   ; Uso de la interrupcion 21h 09h, espera en DX la posicion de memoria de la variable a imprimir.   
+
+   mov ah, 09h
+   lea dx, string
+   int 21h
